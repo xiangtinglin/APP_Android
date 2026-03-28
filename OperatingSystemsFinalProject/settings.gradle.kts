77e9e4a47b189a52c18771b4bff5adaa6c609f66
林湘婷 <116503010+xiangtinglin@users.noreply.github.com>
2026-03-29 01:11:32 +0800

    Add files via upload

---
+++ OperatingSystemsFinalProject/settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        repositories {
+            google()
+            mavenCentral()
+        }
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+
+rootProject.name = "Android_OS_FinalReport"
+include(":app")
+ 
